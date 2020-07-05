d33d76943fb4cd17fd4b59e0dcbc3ece76fbcaa2
ANanade <nanade.archana@gmail.com>
2020-07-06 01:38:36 +0530

    Code file created.

---
+++ code.py
@@ -0,0 +1,81 @@
+# --------------
+#import the packages 
+import pandas as pd
+import numpy as np
+import os
+import seaborn as sns
+import matplotlib.pyplot as plt
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import SelectFromModel      
+from sklearn.feature_selection import RFE
+from sklearn.metrics import f1_score
+from sklearn.metrics import accuracy_score,classification_report,confusion_matrix
+
+# code starts here
+
+seg=pd.read_csv(path,encoding='latin1')
+
+#categorical columns
+categorical = seg.select_dtypes(include='object')
+
+#numerical columns
+numerical = seg._get_numeric_data()
+
+# concat the numerical variable and categorical columns
+df = pd.concat([seg[numerical.columns.tolist()],pd.get_dummies(seg[categorical.columns.tolist()])],axis=1)
+df.head()
+
+# code ends here
+
+
+# --------------
+# code starts here
+X = df.drop(['customer id', 'segments'], axis = 1)
+y = df['segments']
+X_train, X_test, y_train,y_test = train_test_split(X,y,test_size = 0.3, random_state = 42, shuffle = True) 
+classifier = xgb.XGBClassifier(random_state = 2)
+classifier.fit(X_train, y_train)
+y_pred = classifier.predict(X_test)
+
+f1 = f1_score(y_test,y_pred,average='macro')
+print("F1-scpore:",f1)
+xgb_cr = classification_report(y_test,y_pred)
+print("Classification Report:/n",xgb_cr)
+# code ends here
+
+
+# --------------
+from sklearn.model_selection import GridSearchCV
+parameters={'learning_rate':[0.1,0.15,0.2,0.25,0.3],
+            'max_depth':range(1,3)}
+
+
+# code start here
+grid_search = GridSearchCV(estimator = classifier, param_grid = parameters , n_jobs = -1, verbose = 4)
+
+grid_search.fit(X_train, y_train)
+grid_predictions = grid_search.predict(X_test)
+
+grid_f1 = f1_score(grid_predictions, y_test, average = 'macro')
+
+report = classification_report(y_test,grid_predictions)
+# code ends here
+
+
+# --------------
+from sklearn.ensemble import RandomForestClassifier
+
+
+# code starts here
+model = RandomForestClassifier(random_state = 2)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+f1 = f1_score(y_test,y_pred,average='macro')
+print(f1)
+report = classification_report(y_test,y_pred)
+print(report)
+# code ends here
+
+
